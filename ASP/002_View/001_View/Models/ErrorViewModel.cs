4c7a383273cddaa3d8d8785fec99145bafd726df
DruiD <omsk.petr@gamil.com>
2022-02-25 20:51:47 +0600

    Add Examles of views.

---
+++ ASP/002_View/001_View/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _001_View.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
